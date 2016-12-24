df83315f9aee360bbefafac5977e493f38b78cd7
Fallentol <lugvas1@gmail.com>
2016-12-24 14:13:13 +0200

    first update

---
+++ src/main/java/tests/TestClass.java
@@ -0,0 +1,12 @@
+package tests;
+
+/**
+ * Created by New on 12/24/2016.
+ */
+public class TestClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
